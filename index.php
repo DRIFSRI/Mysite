35783a220f2590d4d4d9442334fdc037e444b25d
DRIFSRI <56934396+DRIFSRI@users.noreply.github.com>
2021-01-28 20:57:11 +0300

    init version

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	    <link href="/styles.css" rel="stylesheet">
+	    <title>
+	    	Мой сайт
+	    </title>
+	</head>
+
+	<body>
+		<div class="header">
+			<div class="clearfix">
+				<p>Hello world;</p>
+			</div>
+		</div>
+	</body>
+</html> 
